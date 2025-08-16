bf6b2f019cab06aadd020dc799a37f2a16c1bfb2
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2025-08-16 15:24:46 +0100

    Refactor: Remove TestimonialsSection and implement MarketInsightsSection with marquee effect
    Add: Create AboutSection with responsive design and GSAP animations
    
    Add: Implement ProblemsSection to highlight key challenges faced by investors and startups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,24 @@
-import Header from "../components/Header";
-import HeroSection from "../components/HeroSection";
-import FeaturesSection from "../components/FeaturesSection";
-import StickySection from "../components/StickySection";
-import EcosystemSection from "../components/EcosystemSection";
-import TrustComplianceSection from "../components/TrustComplianceSection";
-import OrchestrationSection from "../components/OrchestrationSection";
-import BountySection from "../components/BountySection";
-import FaqSection from "../components/FaqSection";
-import AdditionalFeaturesSection from "../components/AdditionalFeaturesSection";
-import TestimonialsSection from "../components/TestimonialsSection";
-import PricingSection from "../components/PricingSection";
-import BlogSection from "../components/BlogSection";
-import NewsletterSection from "../components/NewsletterSection";
-import Footer from "../components/Footer";
+import Header from '../components/Header'
+import HeroSection from '../components/HeroSection'
+import ProblemsSection from '../components/ProblemsSection'
+import AboutSection from '../components/AboutSection'
+import AdditionalFeaturesSection from '../components/AdditionalFeaturesSection'
+import MarketInsightsSection from '../components/MarketInsightsSection'
+import PricingSection from '../components/PricingSection'
+import BlogSection from '../components/BlogSection'
+import NewsletterSection from '../components/NewsletterSection'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-black text-white">
       <Header />
       <HeroSection />
-      <EcosystemSection />
-      <FeaturesSection />
-      <StickySection />
-      <BountySection />
-      <OrchestrationSection />
-      <TrustComplianceSection />
+      <ProblemsSection />
+      <AboutSection />
       <AdditionalFeaturesSection />
-      <TestimonialsSection />
+      <MarketInsightsSection />
       <PricingSection />
-      <FaqSection />
       <BlogSection />
       <NewsletterSection />
       <Footer />
