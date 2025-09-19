27e33f9c464d4de25d504fd3fd35f5c83ff94b7d
bedouijesser <bedouijesser@gmail.com>
2025-09-19 14:07:55 +0100

    feat: add LinkedIn Pixel tracking script for enhanced analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -147,6 +147,37 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        {/* End Meta Pixel Code */}
+        {/* Linkedin Pixel Code */}
+        <Script
+          id="linkedin-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "8697081";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+              </script><script type="text/javascript">
+              (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8697081&fmt=gif"
+          />
+        </noscript>
 
         <Script id="critical-sw-registration" strategy="lazyOnload">
           {`if ('serviceWorker' in navigator) {
